432a8fbcc9d0741216f70b9e62b5452646d77ab9
Reko <36913@ma-web.nl>
2024-03-07 09:52:27 +0100

    les `1

---
+++ Arcade-Mechs/Assets/Scripts/Idle.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Idle : MonoBehaviour
+{
+    Animator m_Animator;
+
+    void Start()
+    {
+        //Get the Animator attached to the GameObject you are intending to animate.
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //Press the up arrow button to reset the trigger and set another one
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //Reset the "Crouch" trigger
+            //  Debug.Log("DownArrow");
+            m_Animator.SetTrigger("Catwalk");
+
+            //Send the message to the Animator to activate the trigger parameter named "Jump"
+            m_Animator.ResetTrigger("Breakdance");
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //Reset the "Jump" trigger
+            //Debug.Log("UpArrow");
+            m_Animator.SetTrigger("Breakdance");
+
+            //Send the message to the Animator to activate the trigger parameter named "Crouch"
+            m_Animator.ResetTrigger("Catwalk");
+        }
+    }
+}
